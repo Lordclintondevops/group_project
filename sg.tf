c8113020b8ead8d88107c53c6636a8a768d95209
Lord Clinton Devops/Cloud Engineer <139438345+Lordclintondevops@users.noreply.github.com>
2024-05-20 22:34:14 +0100

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "group_sg" {
     ingress {
         from_port = 22
         to_port = 22
-        protocol = "ssh"
+        protocol = "tcp"
         cidr_blocks = ["10.1.0.0/24"]
     }
 
